52dee03fa9d5275ded45b00b73296ffde6b8526b
cooldev225 <goldsatr22501@gmail.com>
2023-03-28 06:22:59 -0600

    add User, Message class

---
+++ src/Message.php
@@ -0,0 +1,48 @@
+<?php
+    require __DIR__ . './User.php';
+    require __DIR__ . './constants/MessageType.php';
+    require __DIR__ . './constants/UserType.php';
+
+    class Message {
+        private User $sender;
+        private User $receiver;
+        private string $text;
+        private DateTime $createdAt;
+        private MessageType $type;
+
+        public function __construct(User $sender, User $receiver, string $text, MessageType $type) {
+            $this->sender = $sender;
+            $this->receiver = $receiver;
+            $this->text = $text;
+            $this->createdAt = new DateTime('now');
+            $this->type = $type;
+        }
+
+        public function getFullNameOfSender() {
+            return $this->sender->getFullName();
+        }
+
+        public function getFullNameOfReceiver() {
+            return $this->receiver->getFullName();
+        }
+
+        public function getText() {
+            return $this->text;
+        }
+
+        public function getType() {
+            return $this->type;
+        }
+
+        public function getCreatedAt() {
+            return $this->createdAt;
+        }
+
+        public function saveMessage() {
+            if ($this->type == MessageType::System) {
+                if ($this->sender->getUserType() == UserType::Teacher && $this->receiver->getUserType() == UserType::Student) return true; else return false;
+            } else {
+                if ($this->sender->getUserType() != UserType::Teacher && $this->receiver->getUserType() != UserType::Teacher) return false; else return true;
+            }
+        }
+    }
---
+++ src/User.php
@@ -0,0 +1,70 @@
+<?php
+    require __DIR__ . './constants/UserType.php';
+    require __DIR__ . './constants/constants.php';
+    require __DIR__ . './Message.php';
+
+    class User {
+        private int $id;
+        private string $firstName;
+        private string $lastName;
+        private string $email;
+        private string $avatarUrl;
+        private string $salutation;
+        private UserType $userType;
+
+        public function __construct(UserType $userType, int $id, string $firstName, string $lastName, string $email, string $avatarUrl = DEFAULT_AVATAR_URL, string $salutation = null) {
+            $this->userType = $userType;
+            $this->id = $id;
+            $this->firstName = $firstName;
+            $this->lastName = $lastName;
+            $this->email = $email;
+            $this->avatarUrl = $avatarUrl;
+            switch ($userType) {
+                case UserType::Parent:
+                case UserType::Teacher:
+                    $this->salutation = $salutation;
+                    break;
+                case UserType::Student:
+                default:
+                    break;
+            }
+        }
+
+        public function getFullName() {
+            switch ($this->userType) {
+                case UserType::Parent:
+                case UserType::Teacher:
+                    return "{$this->salutation}, {$this->firstName} {$this->lastName}";
+                case UserType::Student:
+                    return "{$this->firstName} {$this->lastName}";
+                default:
+                    break;
+            }
+        }
+
+        public function getAvatarUrl() {
+            return $this->avatarUrl;
+        }
+
+        public function getId() {
+            return $this->id;
+        }
+
+        public function getEmail() {
+            return $this->email;
+        }
+
+        public function saveUser() {
+            if ($this->email && str_ends_with($this->avatarUrl, '.jpg')) return true;
+            return false;
+        }
+
+        public function getUserType() {
+            return $this->userType;
+        }
+
+        public function sendMessage(User $receiver, string $text, MessageType $type) {
+            $message = new Message($this, $receiver, $text, $type);
+            return $message->saveMessage();
+        }
+    }
